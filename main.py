ec66348653919803997f0bee9c21b23bc868355f
Ida_Touray <tourayida428@gmail.com>
2022-05-06 15:38:50 -0400

    homepage

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
 
-if __name__ == "__main__":
-    app.run()
+
+
+
+# this routes to the home page
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def cart():
+    return render_template('cart.html')
+
+# @app.route('/cart/')
+# def cart():
+#     return render_template("cart.html")
+
+@app.route('/registration/')
+def registration():
+    return render_template("register.html")
+
+@app.route('/login/')
+def login():
+    return render_template("login.html")
+
+@app.route('/menu/')
+def menu():
+    return render_template("menu.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
